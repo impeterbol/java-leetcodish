26f21d1f0f93a30638a5e9dd45acb362b7d1b97a
Peter <53663056+peterboldyrev@users.noreply.github.com>
2021-02-22 22:48:49 -0800

    Add files via upload

---
+++ transpondArr.java
@@ -0,0 +1,72 @@
+//Andrea Villalobos / Feb 22 // CSS 142
+
+
+import java.net.Socket;
+import java.util.Arrays;
+
+public class AndreaTaskFeb22 {
+    public static void main(String[] args){
+        //    test array
+        int [][] matrix = new int[][] {
+            {  1,   2, 3,   4,   5},
+        {  6,   7,   8,   9, 10}, 
+        { 11, 12, 13, 14, 15}, 
+        { 16, 17, 18, 19, 20},
+        { 21, 22, 23, 24, 25}
+       
+    };
+        // 
+        
+        transpond(matrix, printArr(matrix));
+
+                                             //end of main
+    }
+
+    public static int[] printArr(int[][] someArr){
+        System.out.println();
+        System.out.println("ORIGINAL ARRAY");
+        System.out.println();
+        int r =0;
+        int c = 0;
+        for(int rows=0;rows<someArr.length;rows++){
+            for(int cols=0;cols<someArr[rows].length;cols++){
+                c++;
+                System.out.print(someArr[rows][cols]+" ");
+            }
+            r++;
+            System.out.println(); //goign to the next row
+        }
+        int [] rowsCols = new int[2];
+        rowsCols[0]=r;
+        rowsCols[1]=c/r;
+        System.out.println("There are: rows "+r+" and cols "+c/r);
+        return rowsCols;
+        //end of printarr
+    }
+
+
+    public static int [][] transpond(int[][] someArr, int[] rowsCols){
+        System.out.println();
+        System.out.println("TRANSPONDED");
+        System.out.println();
+       
+        int [][] transp = new int[rowsCols[1]][rowsCols[0]];
+        int newR = rowsCols[1];
+        int newC = rowsCols[0];
+
+        for(int rowS=0,rowN=0;rowS<newR;rowS++,rowN++){
+            for(int colS=0,colN=0;colS<newC;colS++,colN++){
+                // System.out.println("this is col n "+ colN + " this is row n "+ rowN);
+                // System.out.println("this is col s "+ colS + " this is row s "+ rowS);
+                // System.out.println();
+                transp[rowN][colN]=someArr[colS][rowS];
+                    System.out.print(someArr[colS][rowS]+" ");
+                    // System.out.println();
+            }
+            System.out.println();
+        }
+     return transp;
+        //end of transponded
+    }
+    //end of class
+}
